b9b5bbb80cddcebcafb6288027e14ea4d5c0e3b6
tjclawson <tjamesclawson@gmail.com>
2019-08-22 18:57:23 -0400

    part 2 complete

--- app/src/main/java/com/example/android_custom_view_volume/MainActivity.kt
+++ app/src/main/java/com/example/android_custom_view_volume/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.android_custom_view_volume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*var volumeText = "Volume ${VolumeKnob.volume}"
+        var rotateText = "Rotation ${VolumeKnob.rotate}"
+        tv_rotation.text = rotateText
+        tv_volume.text = volumeText*/
+
+
     }
 }
--- app/src/main/java/com/example/android_custom_view_volume/VolumeKnob.kt
+++ app/src/main/java/com/example/android_custom_view_volume/VolumeKnob.kt
@@ -7,17 +7,25 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class VolumeKnob(context: Context?, attrs: AttributeSet?): View(context, attrs) {
 
 
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var startX = 0f
-    private var endX = 0f
-    private var diffX = 0f
-    private var rotate = 0f
+        private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG)
+        private var startX = 0f
+        private var endX = 0f
+        private var diffX = 0f
+        private var rotate = 0f
+        private var volume = 0f
+        private var position = 0f
+        private var maxRotate = 315f
+        private var minRotate = 0f
+
+
 
     init {
 
@@ -33,10 +41,21 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?): View(context, attrs) 
             MotionEvent.ACTION_MOVE -> {
                 endX = event.x
                 diffX = endX - startX
-                rotate = (diffX * 360) / width
+                rotate = position + (diffX * 360) / width
+                if(rotate >= maxRotate) {
+                    rotate = maxRotate
+                } else if(rotate < minRotate) {
+                    rotate = minRotate
+                } else {
+                    rotate = rotate
+                }
+                volume = rotate/315
                 invalidate()
             }
            MotionEvent.ACTION_UP -> {
+               position = rotate
+               println("volume $volume")
+               println("rotation $rotate")
 
            }
 
@@ -55,7 +74,8 @@ class VolumeKnob(context: Context?, attrs: AttributeSet?): View(context, attrs) 
         canvas?.drawCircle(width/2f, height/2f, 400f, paint)
         canvas?.drawCircle(width/2.5f, height/1.5f, 50f, paint2)
 
-        //canvas?.rotate(180f, width/2f, height/2f)
     }
+
+
 
 }
